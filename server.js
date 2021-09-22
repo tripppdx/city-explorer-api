cecf9bd8947b83dc319343898df0391c4f9a8610
Tripp Lucas <tripppdx@gmail.com>
2021-09-22 00:39:55 -0700

    lab 7 card 2.4

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const climate = require('./data/weather.json');
+
 const foods = require('./data.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// middleware
+// middleware - app.use
 app.use(cors());
 
 app.get('/', (request, response) => {
-  response.status(200).send('I am home at last!');
+  response.status(200).send('goto: localhost:3001/weather');
 });
 
 app.get('/foods', (request, response) => {
@@ -23,6 +25,17 @@ app.get('/foods', (request, response) => {
   const firstVeggy = foods.find(food => food.vegetarian === veg);
 
   response.send(firstVeggy);
+});
+
+// WEATHER
+app.get('/weather', (request, response) => {
+  const locationData = climate.find(city => city.city_name === 'Seattle');
+  // const firstClimate = climates.find(climate => climate.searchQuery === data);
+  const Forecast = {
+    date: '',
+    desciption: '',
+  };
+  response.status(200).send(weather);
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
